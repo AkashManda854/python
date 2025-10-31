58701ab7dff8907f0025b1cbb069f442221ceb9c
AkashManda854 <mandaakash33@gmail>
2025-10-31 12:46:24 +0530

    added all information

---
+++ app.py
@@ -0,0 +1,14 @@
+print("hello world")
+a =30
+b =20
+c = True
+d = 3+5j
+e =(1,2,30)
+f =[1,23,3,4]
+print(a+b)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
